84aaec52c2bd559315b483bc7a882c280ad76fc4
eyal.kibbar@gmail.com <eyal.kibbar@gmail.com@6f7e9320-9a2e-3c67-76ab-11ce00100491>
2011-07-24 09:09:00 +0000

    key based routing
    git-svn-id: http://openkad.googlecode.com/svn/trunk@6 6f7e9320-9a2e-3c67-76ab-11ce00100491

--- kbr/src/il/technion/ewolf/kbr/DefaultNodeConnectionListener.java
+++ kbr/src/il/technion/ewolf/kbr/DefaultNodeConnectionListener.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 import java.io.InputStream;
 import java.net.Socket;
 
-public class DefaultNodeConnectionListener implements NodeConnectionListener {
+abstract public class DefaultNodeConnectionListener implements NodeConnectionListener {
 
 	public void onIncomingMessage(String tag, Node from, InputStream in) throws IOException {}
 	public void onIncomingConnection(String tag, Node from, Socket sock) throws IOException {}
