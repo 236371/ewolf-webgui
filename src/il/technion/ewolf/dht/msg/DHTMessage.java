2cfcdeb23bdf26cb5b9f218b9309bf6cc7b15d14
eyal.kibbar@gmail.com <eyal.kibbar@gmail.com@540a3fd9-2bd6-9826-1f0e-7f7ee830f868>
2012-02-19 08:53:31 +0000

    git-svn-id: http://dht.googlecode.com/svn/trunk@3 540a3fd9-2bd6-9826-1f0e-7f7ee830f868

--- src/il/technion/ewolf/dht/msg/DHTMessage.java
+++
@@ -1,9 +0,0 @@
-package il.technion.ewolf.dht.msg;
-
-import java.io.Serializable;
-
-public class DHTMessage implements Serializable {
-
-	private static final long serialVersionUID = -769953987765869304L;
-
-}
