08bf7d09a4d4ade517dadc3c719d642b3b250d03
Anna <send2ann@t2.technion.ac.il>
2012-05-14 10:59:09 +0300

    adding handler for self profile

---
+++ src/il/technion/ewolf/server/SelfProfileHandler.java
@@ -0,0 +1,61 @@
+package il.technion.ewolf.server;
+
+import il.technion.ewolf.WolfPack;
+import il.technion.ewolf.WolfPackLeader;
+import il.technion.ewolf.socialfs.Profile;
+import il.technion.ewolf.socialfs.SocialFS;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.http.HttpException;
+import org.apache.http.HttpRequest;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpStatus;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.protocol.HttpContext;
+import org.apache.http.protocol.HttpRequestHandler;
+
+import com.google.gson.Gson;
+import com.google.inject.Inject;
+
+//@SuppressWarnings("unused")
+public class SelfProfileHandler implements HttpRequestHandler{
+	private String name;
+	private String id;
+	private List<String> groups = new ArrayList<String>();
+	
+	// dependencies
+	private final transient SocialFS socialFS;
+	private final transient WolfPackLeader socialGroupsManager;
+	
+	@Inject
+	public SelfProfileHandler(SocialFS socialFS, WolfPackLeader socialGroupsManager) {
+
+		this.socialFS = socialFS;
+		this.socialGroupsManager = socialGroupsManager;
+		
+		Profile profile = socialFS.getCredentials().getProfile();
+		name = profile.getName();
+		id = profile.getUserId().toString();
+		
+		for (WolfPack w : socialGroupsManager.getAllSocialGroups()) {
+			groups.add(w.toString());
+		}
+	}
+
+	@Override
+	public void handle(HttpRequest req, HttpResponse res,
+			HttpContext context) throws HttpException, IOException {
+		res.addHeader("Server", "e-WolfNode");
+		res.addHeader("Content-Type", "application/json");
+		
+		Gson gson = new Gson();
+		String json = gson.toJson(this);
+		
+		res.setEntity(new StringEntity(json));
+		res.setStatusCode(HttpStatus.SC_OK);				
+	}
+
+}
