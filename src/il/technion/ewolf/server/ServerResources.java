042d611710c2b54b71f92c9893597b738f81c90f
Anna <send2ann@t2.technion.ac.il>
2012-08-09 09:22:51 +0300

    synchronize access to configuration file in ServerResources

--- src/il/technion/ewolf/server/ServerResources.java
+++ src/il/technion/ewolf/server/ServerResources.java
@@ -11,10 +11,13 @@ import javax.activation.FileTypeMap;
 import javax.activation.MimetypesFileTypeMap;
 
 import org.apache.commons.configuration.ConfigurationException;
+import org.apache.commons.configuration.Lock;
 import org.apache.commons.configuration.PropertiesConfiguration;
 
 public class ServerResources {	
 	private static final String MIME_TYPES = "/mime.types";
+    /** A lock object for protecting read/write operations to a configuration file. */
+    private static Object configLock = new Lock("ConfigurationFileLock");
 	
 	static class EwolfConfigurations {
 		String username;
@@ -31,12 +34,15 @@ public class ServerResources {
 
 	public static void setUserConfigurations(String configurationFile, String username,
 			String name, String password) throws ConfigurationException {
-		URL configFile = getResource(configurationFile);
 		try {
-			PropertiesConfiguration config = new PropertiesConfiguration(configFile);
-			config.setProperty("username", username);
-			config.setProperty("name", name);
-			config.setProperty("password", password);
+			synchronized (configLock) {
+				URL configFile = getResource(configurationFile);
+				PropertiesConfiguration config = new PropertiesConfiguration(configFile);
+				config.setProperty("username", username);
+				config.setProperty("name", name);
+				config.setProperty("password", password);
+				config.save();
+			}
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 			System.out.println("Can't read configuration file.");
@@ -49,17 +55,19 @@ public class ServerResources {
 		EwolfConfigurations configurations = new EwolfConfigurations();
 
 		try {
-			URL configFile = getResource(configurationFile);
-			PropertiesConfiguration config = new PropertiesConfiguration(configFile);
-			configurations.username = config.getString("username");
-			configurations.password = config.getString("password");
-			configurations.name = config.getString("name");
-			configurations.serverPort = config.getInt("serverPort", 10000);
-			configurations.ewolfPort = config.getInt("ewolfPort", 10300);
+			synchronized (configLock) {
+				URL configFile = getResource(configurationFile);
+				PropertiesConfiguration config = new PropertiesConfiguration(configFile);
+				configurations.username = config.getString("username");
+				configurations.password = config.getString("password");
+				configurations.name = config.getString("name");
+				configurations.serverPort = config.getInt("serverPort", 10000);
+				configurations.ewolfPort = config.getInt("ewolfPort", 10300);
 
-			for (Object o: config.getList("kbr.urls")) {
-				configurations.kbrURIs.add(new URI((String)o));
-			}
+				for (Object o: config.getList("kbr.urls")) {
+					configurations.kbrURIs.add(new URI((String)o));
+				}
+	        }
 		} catch (ConfigurationException e) {
 			e.printStackTrace();
 			System.out.println("Can't read configuration file.");
