ac51d914234d1b72b86d9b24a9999a8376b2b6e5
Anna <send2ann@t2.technion.ac.il>
2012-08-14 11:51:39 +0300

    update AddWolfpackMemberHandler
    also finish updating handlers responses

--- src/il/technion/ewolf/server/EWolfResponse.java
+++ src/il/technion/ewolf/server/EWolfResponse.java
@@ -8,7 +8,7 @@ public class EWolfResponse {
 	public static final String RES_NOT_FOUND = "ITEM_NOT_FOUND";
 	public static final String RES_BAD_REQUEST = "BAD_REQUEST";
 	public static final String RES_INTERNAL_SERVER_ERROR = "INTERNAL_SERVER_ERROR";
-	public static final String RES_GENERIC_ERROR = "UNKNOWN_ERROR";
+	public static final String RES_GENERIC_ERROR = "GENERAL_ERROR";
 	public static final String RES_UNAVAILBLE_REQUEST = "UNAVAILBLE_REQUEST";
 
 	public EWolfResponse(String result) {
--- src/il/technion/ewolf/server/jsonDataHandlers/AddWolfpackMemberHandler.java
+++ src/il/technion/ewolf/server/jsonDataHandlers/AddWolfpackMemberHandler.java
@@ -1,5 +1,10 @@
 package il.technion.ewolf.server.jsonDataHandlers;
 
+import static il.technion.ewolf.server.EWolfResponse.RES_BAD_REQUEST;
+import static il.technion.ewolf.server.EWolfResponse.RES_GENERIC_ERROR;
+import static il.technion.ewolf.server.EWolfResponse.RES_INTERNAL_SERVER_ERROR;
+import static il.technion.ewolf.server.EWolfResponse.RES_NOT_FOUND;
+import static il.technion.ewolf.server.EWolfResponse.RES_SUCCESS;
 import il.technion.ewolf.ewolf.WolfPack;
 import il.technion.ewolf.ewolf.WolfPackLeader;
 import il.technion.ewolf.server.EWolfResponse;
@@ -10,11 +15,12 @@ import il.technion.ewolf.socialfs.UserIDFactory;
 import il.technion.ewolf.socialfs.exception.ProfileNotFoundException;
 import il.technion.ewolf.stash.exception.GroupNotFoundException;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
 import com.google.inject.Inject;
-
-import static il.technion.ewolf.server.EWolfResponse.*;
 
 public class AddWolfpackMemberHandler implements JsonDataHandler {
 	private final SocialFS socialFS;
@@ -30,15 +36,26 @@ public class AddWolfpackMemberHandler implements JsonDataHandler {
 	}
 
 	private static class JsonReqAddWolfpackMemberParams {
-		String wolfpackName;
-		String userID;
+		List<String> wolfpackNames;
+		List<String> userIDs;
 	}
 
 	static class AddWolfpackMemberResponse extends EWolfResponse {
+		List<EWolfResponse> wolfpacksResult;
+		List<EWolfResponse> usersResult;
 		public AddWolfpackMemberResponse(String result) {
 			super(result);
 		}
-		public AddWolfpackMemberResponse() {
+
+		public AddWolfpackMemberResponse(String result, String errorMessage) {
+			super(result, errorMessage);
+		}
+
+		public AddWolfpackMemberResponse(String result, List<EWolfResponse> wolfpacksResult,
+				List<EWolfResponse> usersResult) {
+			super(result);
+			this.usersResult = usersResult;
+			this.wolfpacksResult = wolfpacksResult;
 		}
 	}
 
@@ -57,37 +74,64 @@ public class AddWolfpackMemberHandler implements JsonDataHandler {
 			return new AddWolfpackMemberResponse(RES_BAD_REQUEST);
 		}
 
-		if (jsonReqParams.wolfpackName == null || jsonReqParams.userID==null) {
-			return new AddWolfpackMemberResponse(RES_BAD_REQUEST + 
-					": must specify both wolfpack name and user ID.");
-		}
-		
-		Profile profile;
-		try {
-			UserID uid = userIDFactory.getFromBase64(jsonReqParams.userID);
-			profile = socialFS.findProfile(uid);
-		} catch (ProfileNotFoundException e) {
-			e.printStackTrace();
-			return new AddWolfpackMemberResponse(RES_NOT_FOUND + ": user with given ID wasn't found.");
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-			return new AddWolfpackMemberResponse(RES_BAD_REQUEST + ": illegal user ID.");
+		if (jsonReqParams.wolfpackNames == null || jsonReqParams.wolfpackNames.isEmpty() || 
+					jsonReqParams.userIDs==null || jsonReqParams.userIDs.isEmpty()) {
+			return new AddWolfpackMemberResponse(RES_BAD_REQUEST, 
+					"Must specify both wolfpack name and user ID.");
 		}
 
-		WolfPack socialGroup = socialGroupsManager.findSocialGroup(jsonReqParams.wolfpackName);
-		if (socialGroup == null) {
-			return new AddWolfpackMemberResponse(RES_NOT_FOUND + ": given wolfpack wasn't found.");
+		List<EWolfResponse> wolfpacksResult = new ArrayList<EWolfResponse>();
+		List<EWolfResponse> usersResult = new ArrayList<EWolfResponse>();
+
+		List<Profile> profiles = new ArrayList<Profile>();
+		for (String userID : jsonReqParams.userIDs) {
+			try {
+				UserID uid = userIDFactory.getFromBase64(userID);
+				profiles.add(socialFS.findProfile(uid));
+				usersResult.add(new EWolfResponse());
+			} catch (ProfileNotFoundException e) {
+				e.printStackTrace();
+				usersResult.add(new EWolfResponse(RES_NOT_FOUND,
+						"User with the given ID wasn't found."));
+				continue;
+			} catch (IllegalArgumentException e) {
+				e.printStackTrace();
+				usersResult.add(new EWolfResponse(RES_NOT_FOUND, "Illegal user ID."));
+				continue;
+			}
 		}
-		try {
-			socialGroup.addMember(profile);
-			//TODO check if allowed to add member several times
-			socialGroupsManager.findSocialGroup("wall-readers").addMember(profile);
-		} catch (GroupNotFoundException e) {
-			e.printStackTrace();
-			return new AddWolfpackMemberResponse(RES_INTERNAL_SERVER_ERROR);
+
+		for (String wolfpackName : jsonReqParams.wolfpackNames) {
+			WolfPack socialGroup = socialGroupsManager.findSocialGroup(wolfpackName);
+			if (socialGroup == null) {
+				wolfpacksResult.add(new EWolfResponse(RES_NOT_FOUND,
+						"Given wolfpack wasn't found."));
+				continue;
+			}
+			try {
+				for (Profile profile : profiles) {
+					socialGroup.addMember(profile);
+					//TODO check if allowed to add member several times
+					socialGroupsManager.findSocialGroup("wall-readers").addMember(profile);
+				}
+				wolfpacksResult.add(new EWolfResponse());
+			} catch (GroupNotFoundException e) {
+				e.printStackTrace();
+				wolfpacksResult.add(new EWolfResponse(RES_INTERNAL_SERVER_ERROR));
+			}
 		}
-		
-		return new AddWolfpackMemberResponse();
+
+		for (EWolfResponse res : wolfpacksResult) {
+			if (res.getResult() != RES_SUCCESS) {
+				return new AddWolfpackMemberResponse(RES_GENERIC_ERROR, wolfpacksResult, usersResult);
+			}
+		}
+		for (EWolfResponse res : usersResult) {
+			if (res.getResult() != RES_SUCCESS) {
+				return new AddWolfpackMemberResponse(RES_GENERIC_ERROR, wolfpacksResult, usersResult);
+			}
+		}
+		return new AddWolfpackMemberResponse(RES_SUCCESS);
 	}
 
 }
