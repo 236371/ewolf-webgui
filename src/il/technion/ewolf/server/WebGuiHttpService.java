e415e035067511cc2d15aa93c62189d6d7d49a83
Anna <send2ann@t2.technion.ac.il>
2012-09-30 09:32:06 +0200

    fix cookie bug in WebGuiHttpService
    (cookie= "session=key", sessionStore saves key)

--- src/il/technion/ewolf/server/WebGuiHttpService.java
+++ src/il/technion/ewolf/server/WebGuiHttpService.java
@@ -51,7 +51,8 @@ public class WebGuiHttpService extends HttpService {
 			Header[] headers = req.getHeaders("Cookie");
 			for (Header h : headers) {
 				String cookie = h.getValue();
-				if (sessionStore.isValid(cookie)) {
+				String key = cookie.substring("session=".length());
+				if (sessionStore.isValid(key)) {
 					authorized = true;
 					context.setAttribute("authorized", true);
 					break;
