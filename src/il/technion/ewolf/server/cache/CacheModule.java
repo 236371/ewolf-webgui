07c901d02ba8b6e596fea64f03bf821bad272f5a
Anna <send2ann@t2.technion.ac.il>
2012-10-09 23:17:56 +0200

    implement profiles cache (WIP)

--- src/il/technion/ewolf/server/cache/CacheModule.java
+++ src/il/technion/ewolf/server/cache/CacheModule.java
@@ -9,6 +9,9 @@ import il.technion.ewolf.msg.SocialMessage;
 import il.technion.ewolf.posts.Post;
 import il.technion.ewolf.socialfs.Profile;
 import il.technion.ewolf.socialfs.SocialFS;
+import il.technion.ewolf.socialfs.UserID;
+import il.technion.ewolf.socialfs.UserIDFactory;
+import il.technion.ewolf.socialfs.exception.ProfileNotFoundException;
 
 import java.io.FileNotFoundException;
 import java.util.HashMap;
@@ -33,6 +36,7 @@ public class CacheModule extends AbstractModule {
 		defaultProps.setProperty("server.cache.newsfeed.intervalSec", "30");
 		defaultProps.setProperty("server.cache.wolfpacks.intervalSec", "30");
 		defaultProps.setProperty("server.cache.inbox.intervalSec", "30");
+		defaultProps.setProperty("server.cache.profiles.intervalSec", "30");
 
 		return defaultProps;
 	}
@@ -49,6 +53,41 @@ public class CacheModule extends AbstractModule {
 	@Override
 	protected void configure() {
 		Names.bindProperties(binder(), properties);
+	}
+
+	@Provides
+	@Singleton
+	ICacheWithParameter<Map<String,Profile>, String> provideProfilesCache(
+			@Named("server.cache.profiles.intervalSec") int cachedTimeSec,
+			final SocialFS socialFS, final UserIDFactory userIDFactory) {
+		return new SimpleCacheWithParameter<Map<String,Profile>, String>(
+				new ICacheWithParameter<Map<String,Profile>, String>() {
+					Map<String,Profile> profilesCache = new HashMap<String, Profile>();
+
+					{
+						// add self profile to cache
+						profilesCache.put("-1", socialFS.getCredentials().getProfile());
+					}
+
+					@Override
+					public Map<String,Profile> get(String strUid) {
+						if (!profilesCache.containsKey(strUid)) {
+							try {
+								UserID uid = userIDFactory.getFromBase64(strUid);
+								Profile profile = socialFS.findProfile(uid);
+								profilesCache.put(strUid, profile);
+							} catch (ProfileNotFoundException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							} catch (IllegalArgumentException e) {
+								// TODO Auto-generated catch block
+								e.printStackTrace();
+							}
+						}
+
+						return profilesCache;
+					}
+				}, cachedTimeSec);
 	}
 
 	@Provides
