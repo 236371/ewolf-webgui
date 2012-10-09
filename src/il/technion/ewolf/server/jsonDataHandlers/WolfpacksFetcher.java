c0e29b0f27538fef38fca3b06e01db436ef0ccfc
Anna <send2ann@t2.technion.ac.il>
2012-10-10 00:20:57 +0200

    use profilesCache in WolfpacksFetcher

--- src/il/technion/ewolf/server/jsonDataHandlers/WolfpacksFetcher.java
+++ src/il/technion/ewolf/server/jsonDataHandlers/WolfpacksFetcher.java
@@ -6,11 +6,8 @@ import static il.technion.ewolf.server.EWolfResponse.RES_NOT_FOUND;
 import il.technion.ewolf.ewolf.WolfPack;
 import il.technion.ewolf.server.EWolfResponse;
 import il.technion.ewolf.server.cache.ICache;
+import il.technion.ewolf.server.cache.ICacheWithParameter;
 import il.technion.ewolf.socialfs.Profile;
-import il.technion.ewolf.socialfs.SocialFS;
-import il.technion.ewolf.socialfs.UserID;
-import il.technion.ewolf.socialfs.UserIDFactory;
-import il.technion.ewolf.socialfs.exception.ProfileNotFoundException;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,18 +17,16 @@ import com.google.gson.JsonElement;
 import com.google.inject.Inject;
 
 public class WolfpacksFetcher implements JsonDataHandler {
-	private final SocialFS socialFS;
-	private final UserIDFactory userIDFactory;
 
 	private final ICache<List<WolfPack>> wolfpacksCache;
+	private final ICacheWithParameter<Profile, String> profilesCache;
 
 	@Inject
-	public WolfpacksFetcher(SocialFS socialFS, UserIDFactory userIDFactory,
-			ICache<List<WolfPack>> cache) {
-		this.socialFS = socialFS;
-		this.userIDFactory = userIDFactory;
-
-		wolfpacksCache = cache;
+	public WolfpacksFetcher(
+			ICache<List<WolfPack>> wolfpacksCache,
+			ICacheWithParameter<Profile, String> profilesCache) {
+		this.wolfpacksCache = wolfpacksCache;
+		this.profilesCache = profilesCache;
 	}
 
 	private static class JsonReqWolfpacksParams {
@@ -53,7 +48,7 @@ public class WolfpacksFetcher implements JsonDataHandler {
 	}
 	/**
 	 * @param	jsonReq	serialized object of JsonReqWolfpacksParams class
-	 * @return	list of all social groups (wolfpacks) names, the user has access to them
+	 * @return	list of all social groups' (wolfpacks') names, the user has access to them
 	 */
 	@Override
 	public EWolfResponse handleData(JsonElement jsonReq) {
@@ -73,20 +68,15 @@ public class WolfpacksFetcher implements JsonDataHandler {
 				groups.add(w.getName());
 			}
 		} else {
-			try {
-				UserID uid = userIDFactory.getFromBase64(jsonReqParams.userID);
-				Profile profile = socialFS.findProfile(uid);
-				for (WolfPack w : wgroups) {
-					if (w.getMembers().contains(profile)) {
-						groups.add(w.getName());
-					}
-				}
-			} catch (ProfileNotFoundException e) {
-				e.printStackTrace();
+			Profile profile = profilesCache.get(jsonReqParams.userID);
+			if (profile == null) {
 				return new WolfpacksResponse(RES_NOT_FOUND);
-			} catch (IllegalArgumentException e) {
-				e.printStackTrace();
-				return new WolfpacksResponse(RES_BAD_REQUEST);
+			}
+
+			for (WolfPack w : wgroups) {
+				if (w.getMembers().contains(profile)) {
+					groups.add(w.getName());
+				}
 			}
 		}
 		return new WolfpacksResponse(groups);
