480046a3ea7a8f73e9db11e591ba241b4526b9c8
Anna <send2ann@t2.technion.ac.il>
2012-11-04 09:08:47 +0200

    rm commented debug printouts

--- src/il/technion/ewolf/server/cache/CacheModule.java
+++ src/il/technion/ewolf/server/cache/CacheModule.java
@@ -152,16 +152,6 @@ public class CacheModule extends AbstractModule {
 						for (Map.Entry<String,WolfPack> entry : wolfpacksMap.entrySet()) {
 							membersMap.put(entry.getKey(), entry.getValue().getMembers());
 						}
-						//////////////////////
-//						for (Map.Entry<String, List<Profile>> entry : membersMap.entrySet()) {
-//							String users = "";
-//							for (Profile p: entry.getValue()) {
-//								users += "\"" + p.getName() + "\" [" + p.getUserId().toString() + "], ";
-//							}
-//							System.err.println("CacheModule: wolfpack " + entry.getKey() + ": " +
-//									users);
-//						}
-						/////////////////////
 						return membersMap;
 					}
 
